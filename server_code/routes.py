ee9c79e00d8e013e7e371855d7529ac30bbb944b
Micah Linnemeier <mlinnem@Micahs-MacBook-Pro.local>
2025-03-08 13:07:43 -0600

    More name refactoring. Tested.

--- server_code/routes.py
+++ server_code/routes.py
@@ -11,28 +11,28 @@ routes = Blueprint('routes', __name__, url_prefix='')
 
 
 @routes.route('/')
-def index_route():
+def indexRoute():
     logger.info("Received request for main menu")
     main_menu_rendered = render_template('main_menu.html')
     logger.info("...Main menu returned")
     return main_menu_rendered;
 
 @routes.route('/new_game')
-def new_game_route():
+def newGameRoute():
     logger.info("Received request for new game screen...")
     new_game_screen_rendered = render_template('new_game_screen.html')
     logger.info("...New game screen returned")
     return new_game_screen_rendered;
 
 @routes.route('/load_game')
-def load_game_route():
+def loadGameRoute():
     logger.info("Received request for load game screen...")
     load_game_screen_rendered = render_template('load_game_screen.html')
     logger.info("...Load game screen returned")
     return load_game_screen_rendered;
 
 @routes.route('/game/<conversation_id>')
-def game_route(conversation_id):
+def gameRoute(conversation_id):
     logger.info(f"Received request for game with conversation_id: {conversation_id}")
 
     conversation = getConversation(conversation_id)
@@ -52,7 +52,7 @@ def game_route(conversation_id):
 # Game seed routes
 
 @routes.route('/get_game_world_listings', methods=['GET'])
-def get_seed_listings_route():
+def getSeedListingsRoute():
     logger.info("Received request for game seed listings data...")
     game_seed_listings = getGameSeedListings()    
     logger.info("...Game seed listings returned")
@@ -65,7 +65,7 @@ def get_seed_listings_route():
 # Conversation routes
 
 @routes.route('/advance_conversation', methods=['POST'])
-def advance_conversation_route():
+def advanceConversationRoute():
     user_message_was_persisted = False
     try:
         logger.info("Received request to advance conversation...")
@@ -140,7 +140,7 @@ def advance_conversation_route():
         }), 500
 
 @routes.route('/create_conversation_from_seed', methods=['POST'])
-def create_conversation_from_seed_route():
+def createConversationFromSeedRoute():
     try:
         data = request.get_json()
         seed_id = data.get('seed_id')
@@ -176,7 +176,7 @@ def create_conversation_from_seed_route():
         
         
 @routes.route('/create_conversation', methods=['POST'])
-def create_conversation_route():
+def createConversationRoute():
     try:
         logger.info("Received new conversation creation request...")
         
@@ -202,7 +202,7 @@ def create_conversation_route():
         }), 500
 
 @routes.route('/delete_conversation', methods=['POST'])
-def delete_conversation_route():
+def deleteConversationRoute():
     data = request.get_json()
     conversation_id = data['conversation_id']
     logger.info(f"Received request to delete conversation with id: {conversation_id}")
@@ -216,14 +216,14 @@ def delete_conversation_route():
         return jsonify({'status': 'error', 'message': 'Conversation not found'}), 404
 
 @routes.route('/get_conversation_listings', methods=['GET'])
-def get_conversation_listings_route():
+def getConversationListingsRoute():
     logger.info("Received request for conversation listings...")
     conversation_listings = getConversationListings()
     logger.info("...Conversation listings returned")
     return jsonify({'conversation_listings': conversation_listings})
 
 @routes.route('/get_conversation', methods=['POST'])
-def get_conversation_route():
+def getConversationRoute():
     try:
         data = request.get_json()
         conversation_id = data.get('conversation_id')
